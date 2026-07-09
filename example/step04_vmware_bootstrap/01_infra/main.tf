4a90eb267aa084c0cf62fc1526055169a5321dd3
junhan <tlswnsgks3@gmail.com>
2026-07-09 11:13:52 +0900

    helm 버전 3.0 수정에 따른 문법 교체

--- example/step04_vmware_bootstrap/01_infra/main.tf
+++ example/step04_vmware_bootstrap/01_infra/main.tf
@@ -13,19 +13,19 @@ terraform {
     # terraform 으로 helm chart 를 직접 배포 가능하도록 하는 provider 추가
     helm = {
       source = "hashicorp/helm"
-      version = "~> 2.14"
+      version = "~> 3.0"
     }
   }
 }
 # 클러스터 접속정보 (local k8s 를 바라 보도록 context 가 변경되어 있어야 한다)
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  config_path = pathexpand("~/.kube/config")
 }
 
 # helm provider 가 동작하려면 config 파일 정보를 전달해야 한다. 
 provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
+  kubernetes = {
+    config_path = pathexpand("~/.kube/config")
   }
 }
 
